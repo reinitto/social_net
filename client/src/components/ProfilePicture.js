470bdc300c50aba1859cc43b0bb73cb5213491d9
Labais <reinitto@gmail.com>
2020-08-26 18:20:59 +0200

    add default exports, support for custom classnames and style

--- client/src/components/ProfilePicture.js
+++ client/src/components/ProfilePicture.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
-export const ProfilePicture = ({ src }) => {
+export const ProfilePicture = ({ src, className = "", style = {} }) => {
   const useStyles = makeStyles((theme) => ({
     profileImage: {
       maxWidth: "100%",
       maxHeight: "240px",
       height: "auto",
       borderRadius: "25%",
+      ...style,
     },
   }));
   const classes = useStyles();
   return (
     <div>
-      <img className={classes.profileImage} src={src} alt="profile" />
+      <img
+        className={`${classes.profileImage} ${className}`}
+        src={src}
+        alt="profile"
+      />
     </div>
   );
 };
+
+export default ProfilePicture;
