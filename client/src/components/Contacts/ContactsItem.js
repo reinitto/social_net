15ff938cb4c3f8b756e54008cf6f74f389fec861
Labais <reinitto@gmail.com>
2020-09-13 22:12:12 +0200

    add link to contacts profile, button to chat, indicate new messages

--- client/src/components/Contacts/ContactsItem.js
+++ client/src/components/Contacts/ContactsItem.js
@@ -1,20 +1,52 @@
 import React, { useState, useEffect } from "react";
-import { Button, makeStyles } from "@material-ui/core";
+import { Button, Tooltip, makeStyles } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import ChatIcon from "@material-ui/icons/Chat";
 import { UserAvatarAndName } from "../Friends/UserAvatarAndName";
 import { useUser } from "../../context/user";
 import { useConversations } from "../../context/conversations";
 import calculateDmId from "../utils/calculateDmId";
-const useContactItemStyles = makeStyles((theme) => ({
-  contactItemContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  avatarContainer: {
-    display: "flex",
-    alignItems: "center",
-  },
-}));
+const useContactItemStyles = makeStyles((theme) => {
+  return {
+    contactItemContainer: {
+      display: "inline-flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    chatButton: {
+      display: "flex",
+      alignItems: "center",
+    },
+    avatarContainer: {
+      display: "inline-flex",
+      alignItems: "center",
+    },
+    link: {
+      display: "inline",
+      textDecoration: "none",
+      color: "inherit",
+      "&:hover": {
+        textDecoration: "underline",
+      },
+    },
+  };
+});
+
+const useChatIconStyle = (newMessages) =>
+  makeStyles((theme) => ({
+    chatIcon: {
+      margin: theme.spacing(0.5),
+      padding: theme.spacing(1),
+      color: theme.palette.common.white,
+      boxSizing: "content-box",
+      backgroundColor: newMessages
+        ? theme.palette.error.light
+        : theme.palette.primary.main,
+      borderRadius: "50%",
+      width: 10,
+      height: 10,
+    },
+  }))();
 
 export function ContactsItem({ contact }) {
   const classes = useContactItemStyles();
@@ -22,7 +54,8 @@ export function ContactsItem({ contact }) {
   const { userId } = useUser();
   const [chatId] = useState(calculateDmId(userId, contact._id));
   const [newMessages, setNewMessages] = useState(false);
-
+  const chatIconClasses = useChatIconStyle(newMessages);
+  console.log(chatIconClasses);
   useEffect(() => {
     setNewMessages(hasNewMessages(chatId));
   });
@@ -31,13 +64,20 @@ export function ContactsItem({ contact }) {
     openChat(chatId);
   };
   return (
-    <Button onClick={addConversation} className={classes.contactItemContainer}>
-      <UserAvatarAndName
-        {...contact}
-        containerClassNames={classes.avatarContainer}
-      />
-      <div>{newMessages ? "new messages" : "no"}</div>
-    </Button>
+    <div className={classes.contactItemContainer}>
+      <Link to={`/profile/${contact._id}`} className={classes.link}>
+        <UserAvatarAndName
+          {...contact}
+          containerClassNames={classes.avatarContainer}
+        />
+      </Link>
+      <Tooltip title={newMessages ? "New messages" : "No new Messages"}>
+        <Button onClick={addConversation} className={classes.chatButton}>
+          <ChatIcon className={chatIconClasses.chatIcon} />
+          {/* <div>{newMessages ? "new messages" : "no"}</div> */}
+        </Button>
+      </Tooltip>
+    </div>
   );
 }
 
